c5e3cfe363fd8812f57cd95183c48646d2dbdf81
newbiecihuy <newbiecihuy@gmail.com>
2021-03-31 19:56:33 +0700

    Update bilangan.c

--- bilangan.c
+++ bilangan.c
@@ -1,11 +1,12 @@
+//  jar uf catalina.jar org/apache/catalina/util/ServerInfo.properties
 #include <stdio.h>
 
 int  main()
 {
-        int a,x,nilai;
-//         printf("Masukkan angka : ");
-//         scanf("%d", &a);
-	a =10;
+        int a=0,x=0,nilai=0;
+        // printf("Masukkan angka : ");
+        // scanf("%d", &a);
+        a=10;
     if(a <= 1) {
 	     printf("Masukkan angka mulai dari 2.");
 	  }else{
@@ -16,12 +17,17 @@ int  main()
         	nilai = x;
         if(nilai % 2==1){
              printf("%d adalah bilangan ganjil\n", nilai);
+          if (nilai <= 10) {
           if (nilai == 3 || nilai % nilai ==0 && nilai % 3 !=0 ){//formula bilangan prima
 	           printf("%d adalah bilangan prima\n", nilai);
             } 
+            } else if (nilai > 10) {
+                                if (nilai == 3 || nilai % nilai == 0 && nilai % 3 != 0 && nilai % 5 != 0 && nilai % 7 != 0) {
+                                    printf("%d adalah bilangan prima \n", nilai);
+                                }
+                            }
 
-          }
-         else{
+          }else {
             printf("%d adalah bilangan genap\n", nilai);
 	        if (nilai == 2 ){
 	           printf("%d adalah bilangan prima \n", nilai);
