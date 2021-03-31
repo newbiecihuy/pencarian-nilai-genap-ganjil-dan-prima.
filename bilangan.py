92a02c69b3075cde946264bea841ce0c3a675201
newbiecihuy <newbiecihuy@gmail.com>
2021-03-31 19:10:17 +0700

    Add files via upload

---
+++ bilangan.py
@@ -0,0 +1,11 @@
+angka = 10
+for x in range(2,angka):
+	# print(x)
+	  if x % 2 == 1:
+	    	print "%i adalah bilangan ganjil" % x
+	  	if x == 3 or x % x ==0 and x % 3 !=0:
+	  		 print "%i adalah bilangan prima" % x
+	  else:
+	       print "%i adalah bilangan genap" % x	
+	       if x == 2:   	 
+	        	print "%i adalah bilangan prima" % x
