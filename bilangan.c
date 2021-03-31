1873a9814f467449de0ce54b6b7faff497c35a06
newbiecihuy <newbiecihuy@gmail.com>
2021-03-31 16:32:48 +0700

    Add files via upload

---
+++ bilangan.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int  main()
+{
+        int a,x,nilai;
+        printf("Masukkan angka : ");
+        scanf("%d", &a);
+    if(a <= 1) {
+	     printf("Masukkan angka mulai dari 2.");
+	  }else{
+	   if(a == 2){
+	   printf("%d merupakan bilangan prima.", a);
+	  }else{
+        for(x=2; x < a; x++){
+        	nilai = x;
+        if(nilai % 2==1){
+             printf("%d adalah bilangan ganjil\n", nilai);
+          if (nilai == 2 || nilai == 3 || nilai % nilai ==0 && nilai % 3 !=0 ){//formula bilangan prima
+	           printf("%d adalah bilangan prima\n", nilai);
+            } 
+
+          }
+         else if (nilai % 2==0){
+            printf("%d adalah bilangan genap\n", nilai);
+	        if (nilai == 2 ){
+	           printf("%d adalah bilangan prima \n", nilai);
+            }
+          }
+         
+      }
+    }
+  }
+}	
