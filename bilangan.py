8e6acd26305d5890f2d1382b37cc23992f67e5b5
newbiecihuy <newbiecihuy@gmail.com>
2021-03-31 20:20:53 +0700

    Update bilangan.py

--- bilangan.py
+++ bilangan.py
@@ -1,11 +1,18 @@
-angka = 10
-for x in range(2,angka):
-	# print(x)
-	  if x % 2 == 1:
-	    	print "%i adalah bilangan ganjil" % x
-	  	if x == 3 or x % x ==0 and x % 3 !=0:
-	  		 print "%i adalah bilangan prima" % x
-	  else:
-	       print "%i adalah bilangan genap" % x	
-	       if x == 2:   	 
-	        	print "%i adalah bilangan prima" % x
+__author__ = "newbiecihuy"
+__date__ = "$Mar 31, 2021 8:08:57 PM$"
+
+angka = 100
+for x in range(2, angka):
+    # print(x)
+        if x % 2 == 1:
+            print "%i adalah bilangan ganjil" % x
+            if x <= 10:
+                if x == 3 or x % x == 0 and x % 3 != 0:
+                    print "%i adalah bilangan prima" % x
+            else :
+             if x == 3 or x % x == 0 and x % 3 != 0 and x % 5 != 0 and x % 7 != 0:
+                  print "%i adalah bilangan prima" % x
+        else:
+            print "%i adalah bilangan genap" % x	
+            if x == 2:   	 
+                print "%i adalah bilangan prima" % x
