02e503e8850a53b1cc545ba9605ac5b011012293
newbiecihuy <newbiecihuy@gmail.com>
2022-09-29 11:20:57 +0700

    Update bilangan.py

--- bilangan.py
+++ bilangan.py
@@ -5,14 +5,14 @@ angka = 100
 for x in range(2, angka):
     # print(x)
         if x % 2 == 1:
-            print "%i adalah bilangan ganjil" % x
+            print ("%i adalah bilangan ganjil" % x)
             if x <= 10:
                 if x == 3 or x % x == 0 and x % 3 != 0:
-                    print "%i adalah bilangan prima" % x
+                    print ("%i adalah bilangan prima" % x)
             else :
              if x == 3 or x % x == 0 and x % 3 != 0 and x % 5 != 0 and x % 7 != 0:
-                  print "%i adalah bilangan prima" % x
+                  print ("%i adalah bilangan prima" % x)
         else:
-            print "%i adalah bilangan genap" % x	
+            print ("%i adalah bilangan genap" % x)	
             if x == 2:   	 
-                print "%i adalah bilangan prima" % x
+                print ("%i adalah bilangan prima" % x)
